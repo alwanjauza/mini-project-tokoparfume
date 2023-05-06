53dae9d15e0553b7a76926119ed5c54ccac76a8d
alwanjauza <92279555+alwanjauza@users.noreply.github.com>
2023-05-06 09:42:52 +0700

    update shop page

--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -2,47 +2,46 @@ import { Card, Col, Row, Typography } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { GetNakedZoom, MidnightHazeZoom, SecondSkinZoom } from '../../assets';
-import './shopPage.css'
+import './shopPage.css';
 import { useQuery } from '@apollo/client';
 import { GET_PRODUCT } from '../dashboardPage/query/product-query';
 
-const { Meta } = Card
+const { Meta } = Card;
 
 const ShopPage = () => {
-    const { Text } = Typography
+  const { Text } = Typography;
 
-    // GET DATA
+  // GET DATA
   const {
     data: productData,
     loading: isProductLoading,
     error: productError,
   } = useQuery(GET_PRODUCT);
 
-    return (
-        <>
-        <div className='shop-page'>
+  return (
+    <>
+      <div className="shop-page">
+        <Row gutter={[0, 40]}>
           {productData?.products?.map((item, index) => (
-            <Row gutter={16}>
-            <Col span={8}>
+            <Col span={8} key={index}>
               <Card
-              hoverable
-              style={{
-                width: 240
-              }}
-              cover={<img alt={item.image} src={item.image} />}
-              key={index}
+                hoverable
+                style={{
+                  width: 240,
+                }}
+                cover={<img alt={item.image} src={item.image} />}
               >
-                <Meta 
-                title={item.productName}
-                description={item.productPrice}
+                <Meta
+                  title={item.productName}
+                  description={item.productPrice}
                 />
               </Card>
             </Col>
-          </Row>
           ))}
-        </div>
-        </>
-    );
-}
+        </Row>
+      </div>
+    </>
+  );
+};
 
 export default ShopPage;
